e22af7491e754bc6a21baa59bc727a0bedcee8b2
doggycoder <1558183202@qq.com>
2017-04-09 23:02:09 +0800

    AAC 解码、保存成功。

--- ffmpeg/src/main/cpp/AACDecoder.cpp
+++ ffmpeg/src/main/cpp/AACDecoder.cpp
@@ -38,11 +38,11 @@ int AACDecoder::start() {
     audioFormat=param->format;
     frameSize= (size_t) param->frame_size;
     bytesPerSample = (size_t) av_get_bytes_per_sample(avCodecContext->sample_fmt);
-
     avPacket=av_packet_alloc();
     av_init_packet(avPacket);
     avFrame=av_frame_alloc();
-    av_log(NULL,AV_LOG_DEBUG," start success");
+
+    av_log(NULL,AV_LOG_DEBUG," start success,%d",bytesPerSample);
     return 0;
 }
 
@@ -62,17 +62,25 @@ int AACDecoder::output(uint8_t *data) {
         return ret;
     }
     ret=avcodec_receive_frame(avCodecContext,avFrame);
+    bytesPerSample = (size_t) av_get_bytes_per_sample(avCodecContext->sample_fmt);
     if(ret==0){
+        //PCM采样数据的排列方式，一般是交错排列输出，AVFrame中存储PCM数据各个通道是分开存储的
+        //所以多通道的时候，需要根据PCM的格式和通道数，排列好后存储
         if(channelCount>1){
+            //多通道的
             for (int i = 0; i < frameSize; i++) {
-                for (int j=0;j<channelCount;j++){
+                for (int j=0;j< channelCount;j++){
                     memcpy(data+(i*channelCount+j)*bytesPerSample, avFrame->data[j]+i*bytesPerSample,bytesPerSample);
+                    fwrite(avFrame->data[j]+i*bytesPerSample,1,bytesPerSample,file);
                 }
             }
+            av_log(NULL,AV_LOG_DEBUG,"avcodec_receive_frame ok,%d,%d",bytesPerSample*frameSize*2,avFrame->nb_samples);
         }else{
-
+            //单通道的，
+            memcpy(data,avFrame->data[0],frameSize*channelCount*bytesPerSample);
+            fwrite(avFrame->data[0],1,frameSize*bytesPerSample*channelCount,file);
+            av_log(NULL,AV_LOG_DEBUG,"avcodec_receive_frame ok,%d",frameSize*channelCount*bytesPerSample);
         }
-        av_log(NULL,AV_LOG_DEBUG,"avcodec_receive_frame ok,%d,%d",bytesPerSample*frameSize*2,bytesPerSample);
     }else{
         log(ret,"avcodec_receive_frame");
     }
@@ -81,6 +89,7 @@ int AACDecoder::output(uint8_t *data) {
 }
 
 int AACDecoder::stop() {
+    fclose(file);
     avcodec_free_context(&avCodecContext);
     avformat_close_input(&avFormatContext);
     return 0;
--- ffmpeg/src/main/java/edu/wuwang/ffmpeg/demo/AACDecoderDemo.java
+++ ffmpeg/src/main/java/edu/wuwang/ffmpeg/demo/AACDecoderDemo.java
@@ -13,6 +13,7 @@ import edu.wuwang.ffmpeg.R;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 /**
  * Created by aiya on 2017/4/6.
@@ -32,6 +33,7 @@ public class AACDecoderDemo extends AppCompatActivity implements View.OnClickLis
     private int frameSize;
 
     private byte[] tempData;
+    private float[] tempFloatData;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,30 +46,42 @@ public class AACDecoderDemo extends AppCompatActivity implements View.OnClickLis
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.mBtnInfo:
-                audioBufSize = AudioTrack.getMinBufferSize(44100,
-                    AudioFormat.CHANNEL_OUT_STEREO,
-                    AudioFormat.ENCODING_PCM_16BIT);
-                mAudioTrack=new AudioTrack(AudioManager.STREAM_MUSIC, 44100,
-                    AudioFormat.CHANNEL_OUT_STEREO,
-                    AudioFormat.ENCODING_PCM_16BIT,
-                    audioBufSize,
-                    AudioTrack.MODE_STREAM);
+                //play test.pcm
+//                audioBufSize = AudioTrack.getMinBufferSize(8000,
+//                    AudioFormat.CHANNEL_OUT_MONO,
+//                    AudioFormat.ENCODING_PCM_16BIT);
+//                mAudioTrack=new AudioTrack(AudioManager.STREAM_MUSIC,8000,
+//                    AudioFormat.CHANNEL_OUT_MONO,
+//                    AudioFormat.ENCODING_PCM_16BIT,
+//                    audioBufSize,
+//                    AudioTrack.MODE_STREAM);
+//                mAudioTrack.play();
+//                testPlayPcm();
+
+                audioBufSize = AudioTrack.getMinBufferSize(41000,
+                        AudioFormat.CHANNEL_OUT_MONO,
+                        AudioFormat.ENCODING_PCM_FLOAT);
+                mAudioTrack=new AudioTrack(AudioManager.STREAM_MUSIC, 41000,
+                        AudioFormat.CHANNEL_OUT_MONO,
+                        AudioFormat.ENCODING_PCM_FLOAT,
+                        audioBufSize,
+                        AudioTrack.MODE_STREAM);
                 mAudioTrack.play();
-                testPlayPcm();
+                playFloatPCM();
                 break;
             case R.id.mBtnStart:
                 mpeg.start();
                 sampleRate=mpeg.get(FFMpeg.KEY_SAMPLE_RATE);
                 channelCount=mpeg.get(FFMpeg.KEY_CHANNEL_COUNT);
                 audioFormat=mpeg.get(FFMpeg.KEY_AUDIO_FORMAT);
-                frameSize=mpeg.get(FFMpeg.KEY_FRAME_SIZE)*2;
+                frameSize=mpeg.get(FFMpeg.KEY_FRAME_SIZE);
 
                 audioBufSize = AudioTrack.getMinBufferSize(sampleRate,
-                    AudioFormat.CHANNEL_OUT_STEREO,
+                    AudioFormat.CHANNEL_OUT_MONO,
                     AudioFormat.ENCODING_PCM_FLOAT);
-                tempData=new byte[audioBufSize];
+                tempData=new byte[frameSize*channelCount*4];
                 mAudioTrack=new AudioTrack(AudioManager.STREAM_MUSIC, sampleRate,
-                    AudioFormat.CHANNEL_OUT_STEREO,
+                    AudioFormat.CHANNEL_OUT_MONO,
                     AudioFormat.ENCODING_PCM_FLOAT,
                     audioBufSize,
                     AudioTrack.MODE_STREAM);
@@ -80,12 +94,14 @@ public class AACDecoderDemo extends AppCompatActivity implements View.OnClickLis
                     public void run() {
                         while (!isDestoryed){
                             if(mpeg.output(tempData)!=FFMpeg.EOF){
-                                mAudioTrack.write(tempData, 0, tempData.length);
+                                mAudioTrack.write(byte2float(tempData), 0, tempData.length/4,AudioTrack.WRITE_BLOCKING);
+                                Log.d("FFMPEG_LOG_","->"+tempData.length);
                             }else {
                                 break;
                             }
 
                         }
+                        mpeg.stop();
                     }
                 }).start();
                 break;
@@ -93,6 +109,44 @@ public class AACDecoderDemo extends AppCompatActivity implements View.OnClickLis
     }
 
     private void testPlayPcm(){
+        new Thread(new Runnable() {
+            byte[] data1=new byte[audioBufSize*2];
+            File file=new File("/mnt/sdcard/test.pcm");
+            int off1=0;
+            FileInputStream fileInputStream;
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                try {
+                    fileInputStream=new FileInputStream(file);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                while(!isDestoryed){
+                    try {
+                        int len=fileInputStream.read(data1,0,audioBufSize*2);
+                        if(len>0){
+                            off1+=len;
+                            mAudioTrack.write(data1, 0, len);
+                            if(file.length()<=off1){
+                                break;
+                            }
+                        }else{
+                            break;
+                        }
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
+                }
+
+                mAudioTrack.stop();
+            }
+        }).start();
+
+    }
+
+    private void playFloatPCM(){
         new Thread(new Runnable() {
             byte[] data1=new byte[audioBufSize*2];
             File file=new File("/mnt/sdcard/save.pcm");
@@ -112,7 +166,8 @@ public class AACDecoderDemo extends AppCompatActivity implements View.OnClickLis
                         int len=fileInputStream.read(data1,0,audioBufSize*2);
                         if(len>0){
                             off1+=len;
-                            mAudioTrack.write(data1, 0, audioBufSize*2);
+                            mAudioTrack.write(byte2float(data1), 0, len/4,AudioTrack.WRITE_BLOCKING);
+                            Log.e("FFMPEG_LOG_","ll->"+len);
                             if(file.length()<=off1){
                                 break;
                             }
@@ -128,6 +183,24 @@ public class AACDecoderDemo extends AppCompatActivity implements View.OnClickLis
             }
         }).start();
 
+    }
+
+    public float[] byte2float(byte[] b) {
+        if(tempFloatData==null||tempFloatData.length!=b.length/4){
+            tempFloatData=new float[b.length/4];
+        }
+        for (int i=0;i<b.length/4;i++){
+            int l;
+            l = b[i*4];
+            l &= 0xff;
+            l |= ((long) b[i*4+1] << 8);
+            l &= 0xffff;
+            l |= ((long) b[i*4 + 2] << 16);
+            l &= 0xffffff;
+            l |= ((long) b[i*4 + 3] << 24);
+            tempFloatData[i]=Float.intBitsToFloat(l);
+        }
+        return tempFloatData;
     }
 
     @Override
